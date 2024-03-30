10f52959c92032d681880351fd8e69f1315e117d
Week1 <shohruhmirzomahmudovfcps@gmail.com>
2024-03-30 11:44:17 +0500

    1

--- problem1.cpp
+++ problem1.cpp
@@ -1,72 +1,23 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-class Leaf {
-public:
-    string msg;
-    Leaf(string msg) {
-        this->msg = msg;
-    }
-};
-
-class Node {
-public:
-    int value;
-    Node *left;
-    Node *right;
-    Leaf *leftL;
-    Leaf *rightL;
-    Node(int val) {
-        value = val;
-        left = NULL;
-        right = NULL;
-        leftL = NULL;
-        rightL = NULL;
-    }
-};
-
-Node* buildTree() {
-    Node *root = new Node(100);
-    root->left = new Node(50);
-    root->right = new Node(150);
-
-    root->left->left = new Node(25);
-    root->left->right = new Node(75);
-    root->right->left = new Node(125);
-    root->right->right = new Node(175);
-
-    root->left->leftL = new Leaf("Speed");
-    root->left->rightL = new Leaf("Strength");
-    root->right->leftL = new Leaf("Power");
-    root->right->rightL = new Leaf("Vision");
-
-    return root;
-}
-
-string traverseTree(Node *root, int n) {
-    if (root == NULL) {
-        return "Invalid decision tree!";
-    }
-
-    while (root != NULL) {
-        if (n < root->value) {
-            if (root->leftL != NULL)
-                return root->leftL->msg;
-            root = root->left;
-        } else {
-            if (root->rightL != NULL)
-                return root->rightL->msg;
-            root = root->right;
-        }
-    }
-    return "Invalid decision tree!";
+double calculate_an(int n) {
+    if (n == 1)
+        return 4.0;
+    else
+        return (calculate_an(n - 1) - 1) / 2.0;
 }
 
 int main() {
-    Node *root = buildTree();
     int n;
-    cout << "Enter a value: ";
+    cout << "Enter the value of n: ";
     cin >> n;
-    cout << traverseTree(root, n) << endl;
+
+    cout << fixed << setprecision(10);
+
+    double result = calculate_an(n);
+    cout << "a_" << n << " = " << result << endl;
+
     return 0;
 }
